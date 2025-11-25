04bfdab8dc7f879eb6fe2d672ead428acb12f72c
Charles <109361326+CharlesRafael@users.noreply.github.com>
2025-11-24 23:43:27 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Esto hace que los errores de ESLint NO tumben el build en Vercel
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
